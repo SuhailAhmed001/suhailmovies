6c665a357cb488e53b780d7fa04f9dc03c66c223
suhail <suhailcmmb@gmail.com>
2025-07-12 16:49:53 +0530

    chaneses

--- src/App.jsx
+++ src/App.jsx
@@ -87,7 +87,7 @@ const App = () => {
           <img src="./hero-img.png" alt="Hero Banner" />
           <h1>
             Find <span className="text-gradient">Movies</span> You'll Enjoy
-            Without the Suhail
+            Without the Hastle
           </h1>
           <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
         </header>
